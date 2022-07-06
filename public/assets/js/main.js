6677bf3cb5912fdca1e04205f650c56846dba620
Laura Martin gonzalez <lauramartingonzalez@MacBook-Pro-de-Laura.local>
2022-07-06 09:36:32 +0200

    create-const-html

--- public/assets/js/main.js
+++ public/assets/js/main.js
@@ -1,5 +1,8 @@
 'use strict';
 
-console.log('>> Ready :)');
+const write = document.querySelector("js-main")
+const btnTest = document.querySelector("js-btn")
+const pClue = document.querySelector("js-clue")
+const pTry = document.querySelector("js-try")
 
 //# sourceMappingURL=main.js.map
